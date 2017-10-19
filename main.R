9593b09918623f428e2e9de6e57cdf35387a6aeb
roddy_hung <ifferent_4@hotmail.com>
2017-10-19 19:21:56 +0800

    Push the first version shiny codebase template.
      Comment:
          Just the templat which is no any useful code.
    
      type:
          add
    
      issue(No.):
    
      note:
    
      Changes to be committed:
          new file:   main.R

---
+++ main.R
@@ -0,0 +1,27 @@
+
+#############################################################################
+################This is about the Master Thesis Demo program ################
+# Function:                                                                 #
+#    1. Explain what is the frequency domain.                               #
+#    2. Explain what is the Cohrence.                                       #
+# Auth: Roddy_HUng                                                          #
+# Date: 2017.10.19                                                          #
+# Final Version: 0.1
+#############################################################################
+
+
+library(shiny)
+
+ui <- fluidPage(
+
+)
+
+
+server <- function(input, output, session) {
+
+}
+
+shinyApp(ui=ui, server = server)
+
+
+
